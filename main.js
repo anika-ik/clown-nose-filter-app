b9edaa86557622be97d2b9a4232306d27fff18e7
anika-ik <79695696+anika-ik@users.noreply.github.com>
2021-08-16 16:30:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
+
+noseX= 0;
+noseY= 0;
+
 function preload(){
+    clown_nose= loadImage('https://i.postimg.cc/ydFpn1Sk/pngaaa-com-1201409.png');
 
 }
 
 function setup(){
     canvas= createCanvas(300, 300);
     canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
+    image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    image(clown_nose, noseX, noseY, 40, 40);
 
 }
 
 function take_snapshot(){
     save('myfilterimage.png');
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX= results[0].pose.nose.x -17;
+        noseY=  results[0].pose.nose.y - 10;
+        console.log("nose x= " + noseX);
+        console.log("nose y= " + noseY);
+
+    }
 }
